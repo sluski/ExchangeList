d5b25198e23f4951c5daa6c87fc1559885cef729
Przemysław Słupski <przemyslaw@slupski.net>
2019-02-18 21:34:39 +0100

    asd

--- src/main/webapp/resources/js/script.js
+++ src/main/webapp/resources/js/script.js
@@ -1,3 +1,14 @@
+function resizeTableScroll() {
+    var headerHeight = $('.ui-datatable-scrollable-header')[0].scrollHeight;
+//    var footerHeight = $('.ui-datatable-scrollable-footer')[0].scrollHeight;
+    var calculated = window.innerHeight - (headerHeight * 2);
+    console.log("Calculated: " + calculated);
+    console.log("Inner: " + window.innerHeight);
+    console.log("headr: " + headerHeight);
+    console.log("height: " + calculated + ";");
+    $('.ui-datatable-scrollable-body')[0].attributes.style.value = "height: " + calculated + ";";
+}
+
 function onLoadPage() {
     var invisibleButtons = $('.invisibleButton');
     for (var i = 0; i < invisibleButtons.length; i++) {
@@ -8,3 +19,4 @@ function onLoadPage() {
 function cleanNewOrderValues() {
     $('#operations_form\\:operations_accordion_panel\\:resetNewOrderButton').click();
 }
+
