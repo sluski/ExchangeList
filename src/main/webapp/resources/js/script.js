6a2c974d2d2a017ba4a7017cd90eafbbc74ed9b2
Przemysław Słupski <przemyslaw@slupski.net>
2019-02-25 00:28:15 +0100

    view edit

--- src/main/webapp/resources/js/script.js
+++ src/main/webapp/resources/js/script.js
@@ -13,5 +13,21 @@ function cleanNewOrderValues() {
     $('#operations_form\\:operations_accordion_panel\\:resetNewOrderButton').click();
 }
 
+function onProductAddKeyDown(e) {
+    if (e.keyCode === 13) {
+        PrimeFaces.widgets.widget_operations_form_operations_accordion_panel_newProductAddButton.jq.click();
+        PrimeFaces.widgets.widget_operations_form_operations_accordion_panel_newProductNameInput.jq.focus();
+        e.preventDefault();
+    }
+}
+
+function onClientAddKeyDown(e) {
+    if (e.keyCode === 13) {
+        PrimeFaces.widgets.widget_operations_form_operations_accordion_panel_newClientAddButton.jq.click();
+        PrimeFaces.widgets.widget_operations_form_operations_accordion_panel_newClientNameInput.jq.focus();
+        e.preventDefault();
+    }
+}
+
 
 
